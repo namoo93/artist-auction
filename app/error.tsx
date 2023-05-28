04033a2d597c8d8f25af16a60c6bbd1d419ba545
HyoSean <kimhyosun@gmail.com>
2023-05-28 23:44:46 +0900

    add util page

---
+++ app/error.tsx
@@ -0,0 +1,21 @@
+'use client';
+export default function Error({
+  error,
+  reset,
+}: {
+  error: any;
+  reset: () => void;
+}) {
+  console.log('에러', error);
+  return (
+    <>
+      <h4>Error</h4>
+      <button
+        onClick={() => {
+          reset();
+        }}>
+        실행시 페이지 다시 로드
+      </button>
+    </>
+  );
+}
